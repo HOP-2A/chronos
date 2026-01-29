fbb6a5d2dd1eab42a3f610f8c6f2213d77daad68
turuuzbi <turuuzbi@gmail.com>
2026-01-29 15:42:16 +0800

    changed prisma lib because it wasnt working, added company backend

---
+++ app/api/company/route.ts
@@ -0,0 +1,23 @@
+import { prisma } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { name, typeOfCompany, location, feedback, openTime, closeTime } =
+    await req.json();
+  const createdCompany = await prisma.company.create({
+    data: {
+      name,
+      typeOfCompany,
+      location,
+      feedback,
+      openTime,
+      closeTime,
+    },
+  });
+  return NextResponse.json(createdCompany);
+}
+
+export async function GET() {
+  const allCompanies = await prisma.company.findMany({});
+  return NextResponse.json(allCompanies);
+}

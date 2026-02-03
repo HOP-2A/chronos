34e8d2fe3eb64a9e4f56d8a374334ae0b2297fca
Enkhsaihan-lord <enhsaihan.samdam@gmail.com>
2026-02-03 17:27:48 +0800

    save

---
+++ app/createCompany/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+const Page = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#000000]">
+      <div className="justify-center items-center">
+        <p>Hello</p>
+        <div></div>
+      </div>
+    </div>
+  );
+    
+};
+export default Page;

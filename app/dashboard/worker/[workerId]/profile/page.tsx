28f76dea87bd19869588114b85d4030ad71cbee8
Boko3031 <sbolorsaikhan4@gmail.com>
2026-03-04 12:02:16 +0800

    user worker edit delete method

--- app/dashboard/worker/[workerId]/profile/page.tsx
+++ app/dashboard/worker/[workerId]/profile/page.tsx
@@ -1,5 +1,27 @@
 "use client";
+
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { WorkerType } from "../page";
+import Sidebar from "@/app/_component/sidebar";
+
 const Page = () => {
-  return <div className="bg-black">hi</div>;
+  const params = useParams();
+  const workerId = params.workerId;
+  const [worker, setWorker] = useState<WorkerType>();
+  const getOneWorker = async () => {
+    const res = await fetch(`/api/worker/${workerId}`);
+    const response = await res.json();
+    setWorker(response);
+  };
+  console.log(worker);
+  useEffect(() => {
+    getOneWorker();
+  }, []);
+  return (
+    <div>
+
+    </div>
+  );
 };
 export default Page;
--- app/dashboard/worker/[workerId]/settings/page.tsx
+++ app/dashboard/worker/[workerId]/settings/page.tsx
@@ -1,5 +1,19 @@
 "use client";
+
+import { useParams } from "next/navigation";
+import { useState } from "react";
+
 const Page = () => {
+  const params = useParams();
+  const workerId = params.workerId;
+  const [deletedWorker, setDeletedWorker] = useState();
+  const deleteWorker = async () => {
+    const res = await fetch("",{
+      method:"DELETE",
+    });
+    const response = await res.json();
+    setDeletedWorker(response);
+  };
   return <div className="bg-black">hi</div>;
 };
 export default Page;
